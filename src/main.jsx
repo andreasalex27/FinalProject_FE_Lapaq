b27b6a5060011ae5c401eaf387631174aa6945ef
andreasalex27 <albertmunthe27@gmail.com>
2023-11-26 09:15:14 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import SignUp from './pages/signUp/SignUp.jsx'
-import SignIn from './pages/signIn/SignIn.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SignIn />  
+    <BrowserRouter>
+    <App />  
+    </BrowserRouter>
   </React.StrictMode>,
 )
