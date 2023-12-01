4a90a6a3f54d707dc2d1901f0bf9e741e9391e97
Intanrftnh <intannurfathonah46.tedk.a@gmail.com>
2023-12-01 07:15:23 +0700

    modified folder structure

--- src/routes/private/Router.jsx
+++ src/routes/private/Router.jsx
@@ -1,23 +1,11 @@
 import {Route, Routes} from 'react-router-dom';
 import SearchPage from '../../pages/SearchPage/SearchPage';
-import Homepage from '../../pages/Homepage/Homepage';
-import ShopRegister from '../../pages/ShopRegister/ShopRegister';
-import BuyerLayout from '../../layouts/BuyerLayout';
 
 const ROUTE_LIST = [
-    {
-        path: '/homepage',
-        element: <BuyerLayout><Homepage/></BuyerLayout>,
-    },
     {
         path: '/homepage/search',
         element: <SearchPage/>,
     },
-    {
-        path: '/homepage/daftar-toko',
-        element: <ShopRegister />,
-    }    
-
 ];
 
 const RoutePrivate = () => {
